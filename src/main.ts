98f6d8c5f7713fc300e6954ab4f4d5e482974b6b
Matt Gadd <mgadd@sitepen.com>
2016-09-20 14:41:21 +0100

    support args for watch

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,67 @@
 import { Command, Helper } from './interfaces';
+import { Argv } from 'yargs';
+
+export interface BuildArgs extends Argv {
+	watch: boolean;
+	port: number;
+}
 
 const command: Command = {
-	description: 'create a production ready build of your application',
+	description: 'create a build of your application',
 	register(helper) {
+		helper.yargs.option('w', {
+			alias: 'watch',
+			describe: 'watch and serve'
+		});
+
+		helper.yargs.options('p', {
+			alias: 'port',
+			describe: 'port to serve on when using --serve',
+			type: 'number'
+		});
+
 		return helper.yargs;
 	},
-	run(helper: Helper, args: any) {
+	run(helper: Helper, args: BuildArgs) {
 		const webpack: any = require('webpack');
 		const path: any = require('path');
 		const webpackDevServer: any = require('webpack-dev-server');
-
 		const config: any = require('./webpack.config.prod.js');
-		config.devtool = 'eval-source-map';
-		config.entry.unshift(path.join(__dirname, 'node_modules', 'webpack-dev-server/client?'));
 
-		const compiler = webpack(config);
-		const server = new webpackDevServer(compiler, {
+		const options = {
 			stats: {
 				colors: true,
 				chunks: false
 			}
-		});
+		};
 
-		return new Promise((resolve, reject) => {
-			server.listen(9999, '127.0.0.1', (err: Error) => {
-				console.log('Starting server on http://localhost:9999');
-				if (err) {
-					reject(err);
-				}
+		if (args.watch) {
+			config.devtool = 'eval-source-map';
+			config.entry.unshift(path.join(__dirname, 'node_modules', 'webpack-dev-server/client?'));
+		}
+
+		const compiler = webpack(config);
+
+		if (args.watch) {
+			const server = new webpackDevServer(compiler, options);
+
+			return new Promise((resolve, reject) => {
+				const port = args.port || 9999;
+				server.listen(port, '127.0.0.1', (err: Error) => {
+					console.log(`Starting server on http://localhost:${port}`);
+					if (err) {
+						reject(err);
+					}
+				});
 			});
-		});
-
+		} else {
+			return new Promise((resolve) => {
+				compiler.run((err: any, stats: any) => {
+					console.log(stats.toString(options.stats));
+					resolve({});
+				});
+			});
+		}
 	}
 };
 
